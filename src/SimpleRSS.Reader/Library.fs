2b941e314d42055caf02171386302ebad1698551
Juan de Bruin <juan@debruin.dev>
2021-04-24 17:17:45 +0200

    :boom: :art: Swap around feedUrl and pagingOptions

--- src/SimpleRSS.Reader/Library.fs
+++ src/SimpleRSS.Reader/Library.fs
@@ -10,7 +10,7 @@ module Library =
     /// <param name="pagingOpts">Paging options for the feed's items. A page size &lt; 1 will include all items.</param>
     /// <param name="feedUrl">The full URL to the feed document, including or excluding <code>http(s)://</code>.</param>
     /// <remarks>It's unlikely you'd have to use this function directly. See <code>getFeedPaged</code>.</remarks>
-    let getFeedPagedWithReader (reader: IReader) (pagingOpts: PagingOptions) (feedUrl: string) =
+    let getFeedPagedWithReader (reader: IReader) (feedUrl: string) (pagingOpts: PagingOptions) =
         async {
             let! feed =
                 reader.GetAbsoluteUrl feedUrl
@@ -29,8 +29,8 @@ module Library =
     /// <summary>Retrieves the RSS feed document at the given URL including items with paging applied.</summary>
     /// <param name="pagingOpts">Paging options for the feed's items. A page size &lt; 1 will include all items.</param>
     /// <param name="feedUrl">The full URL to the feed document, including or excluding <code>http(s)://</code>.</param>
-    let getFeedPaged pagingOpts feedUrl =
-        getFeedPagedWithReader (new Reader()) pagingOpts feedUrl
+    let getFeedPaged feedUrl pagingOpts =
+        getFeedPagedWithReader (Reader()) pagingOpts feedUrl
 
     /// <summary>
     /// Retrieves the RSS feed document at the given URL including item without any paging applied.
@@ -39,8 +39,8 @@ module Library =
     /// <param name="feedUrl">The full URL to the feed document, including or excluding <code>http(s)://</code>.</param>
     /// <remarks>It's unlikely you'd have to use this function directly. See <code>getFeed</code>.</remarks>
     /// <seealso cref="SimpleRSS.Reader.Library.getFeedPaged" />
-    let getFeedWithReader (reader: IReader) (feedUrl: string) = 
-        getFeedPagedWithReader reader { pageSize = -1; pageNumber = 0 } feedUrl
+    let getFeedWithReader (reader: IReader) (feedUrl: string) =
+        getFeedPagedWithReader reader feedUrl { pageSize = -1; pageNumber = 0 }
 
 
     /// <summary>
@@ -49,4 +49,4 @@ module Library =
     /// <param name="feedUrl">The full URL to the feed document, including or excluding <code>http(s)://</code>.</param>
     /// <seealso cref="SimpleRSS.Reader.Library.getFeedPaged" />
     let getFeed feedUrl =
-        getFeedWithReader (new Reader()) feedUrl
+        getFeedWithReader (Reader()) feedUrl
