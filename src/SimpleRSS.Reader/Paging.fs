d5ac8672a1327fe04505abfbca4eb769e5fc488c
Juan de Bruin <juan@debruin.dev>
2021-04-24 16:06:59 +0200

    :white_check_mark: Add paging tests

--- src/SimpleRSS.Reader/Paging.fs
+++ src/SimpleRSS.Reader/Paging.fs
@@ -1,16 +1,41 @@
 namespace SimpleRSS.Reader
 
+open System
+
 module Paging =
     type PagingOptions =
         {
-            /// <summary>The amount of items to include on each page.</summary>
-            pageSize: int;
+          /// <summary>The amount of items to include on each page.</summary>
+          pageSize: int
 
-            /// <summary>The zero-indexed page number to retrieve.</summary>
-            pageNumber: int
-        }
+          /// <summary>The zero-indexed page number to retrieve.</summary>
+          pageNumber: int }
 
-    let applyPaging (pagingOpts: PagingOptions) (items: _ list) =
+    /// <summary>
+    /// Skips up to <code>amount</code> elements of <code>lst</code>, capping the amount skipped to
+    /// the size of the list.
+    ///
+    /// The reason this function exists is because the stdlib <code>skip</code> function throws an
+    /// exception if amount exceeds the amount of items in the list, which we don't want.
+    /// </summary>
+    /// <param name="amount">Desired amount of elements to skip.</param>
+    /// <param name="lst">The whose elements to skip.</param>
+    /// <returns>A new list with the remaining elements. Might be empty.</returns>
+    let skipSafely (amount: int) (lst: _ list) = List.skip (min amount lst.Length) lst
+
+    /// <summary>
+    /// Takes up to <code>amount</code> elements of <code>lst</code>, capping the amount taken to
+    /// the size of the list.
+    ///
+    /// The reason this function exists is because the stdlib <code>take</code> function throws an
+    /// exception if amount exceeds the amount of items in the list, which we don't want.
+    /// </summary>
+    /// <param name="amount">Desired amount of elements to take.</param>
+    /// <param name="lst">The whose elements to take.</param>
+    /// <returns>A new list with up to <code>amount</code> items. Might be empty.</returns>
+    let takeSafely (amount: int) (lst: _ list) = List.take (min amount lst.Length) lst
+
+    let applyPaging<'a> (pagingOpts: PagingOptions) (items: 'a list) : 'a list =
         items
-        |> List.skip (pagingOpts.pageSize * pagingOpts.pageNumber)
-        |> List.take pagingOpts.pageSize
+        |> skipSafely (pagingOpts.pageSize * pagingOpts.pageNumber)
+        |> takeSafely pagingOpts.pageSize
---
+++ src/SimpleRSS.Reader/Paging.test.fs
@@ -0,0 +1,50 @@
+﻿module SimpleRSS.Reader.Test.Paging
+
+open Xunit
+open SimpleRSS.Reader.Paging
+
+[<Fact>]
+let ``page size 1`` () =
+    let lst = [ 1; 2; 3; 4; 5 ]
+
+    for i in 0 .. (lst.Length - 1) do
+        let expected = [ lst.[i] ]
+        let actual = applyPaging { pageSize = 1; pageNumber = i } lst
+
+        Assert.Equal<int list>(expected, actual)
+
+[<Fact>]
+let ``page size greater than content`` () =
+    let lst = [ 1; 2; 3 ]
+
+    let expected = [ for i in lst do yield i ]
+    let actual = applyPaging { pageSize = 5; pageNumber = 0 } lst
+
+    Assert.Equal<int list>(expected, actual)
+
+[<Fact>]
+let ``page number greater than content`` () =
+    let lst = [ 1; 2; 3 ]
+
+    let expected = []
+    let actual = applyPaging { pageSize = 3; pageNumber = 1 } lst
+
+    Assert.Equal<int list>(expected, actual)
+
+[<Fact>]
+let ``nth page with page size greater than 1`` () =
+    let lst = [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10 ]
+
+    let expected = [ 5; 6 ]
+    let actual = applyPaging { pageSize = 2; pageNumber = 2 } lst
+
+    Assert.Equal<int list>(expected, actual)
+
+[<Fact>]
+let ``last page`` () =
+    let lst = [ 1; 2; 3; 4; 5; 6 ]
+
+    let expected = [ 5; 6 ]
+    let actual = applyPaging { pageSize = 2; pageNumber = 2 } lst
+
+    Assert.Equal<int list>(expected, actual)
